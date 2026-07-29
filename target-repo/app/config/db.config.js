92891a5e90fbecb9596570cc726aff3fb71cd01c
ashana tamrakar <ashanatamrakar2006@gmail.com>
2026-07-30 00:05:55 +0530

    chore: setup dotenv configuration

--- target-repo/app/config/db.config.js
+++ target-repo/app/config/db.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-    url: "mongodb+srv://aaru90043_db_user:THV9vgF2Eio7C058@cluster0.d1yhe48.mongodb.net/notes_db?retryWrites=true&w=majority&appName=ai-coding-agent"
-}
+require('dotenv').config();
+
+const dbUri = process.env.MONGO_URI;
